cb22eca76dd2c70309a846b7aed99e9ba15ada25
Ada_lave <mega.kefi36@gmail.com>
2024-06-30 21:23:27 +0300

    manage admin panel

--- irm_backend/materials/models.py
+++ irm_backend/materials/models.py
@@ -11,6 +11,7 @@ class Theme(models.Model):
     def __str__(self):
         return self.title
 
+
 class Material(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Материал")
@@ -23,22 +24,36 @@ class Material(models.Model):
         verbose_name_plural = "Материалы"
 
 
-
 class FileType(models.Model):
     title = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = "Тип файла"
+        verbose_name_plural = "Тип файлов"
+        
+    def __str__(self):
+        return self.title
+
 
 class Attachment(models.Model):
-    path = models.FileField("uploads/")
+    name = models.CharField(max_length=255, verbose_name="Имя файла")
+    path = models.FileField(upload_to="uploads/", verbose_name="Файл")
     material = models.ForeignKey(
         Material,
         related_name="atachments",
+        verbose_name="К какому материалу",
         on_delete=models.CASCADE,
     )
     file_type = models.ForeignKey(
         FileType,
         related_name="file_type",
+        verbose_name="Тип файла",
         on_delete=models.CASCADE,
     )
 
-
+    class Meta:
+        verbose_name = "Файл"
+        verbose_name_plural = "Файлы"
+        
+    def __str__(self):
+        return self.name
