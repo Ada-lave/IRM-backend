1daf183c7dcdae09d53ca1d3384cce8db29387dc
Ada lave <mega.kefi36@gmail.com>
2024-09-21 13:50:15 +0300

    add export to Excel

--- irm_backend/tests/urls.py
+++ irm_backend/tests/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TestCheckView, TestView
+from .views import *
 urlpatterns = [
     path(f"tests/<int:pk>", TestView.as_view()),
     path(f"tests/<int:pk>/check/", TestCheckView.as_view()),
+    path(f"results/export", ResultView.as_view())
 ]
--- irm_backend/tests/views.py
+++ irm_backend/tests/views.py
@@ -1,13 +1,42 @@
 from rest_framework import generics, views
-from .models import Test, Answer
+from .models import Test, Answer, Result
 from rest_framework.response import Response
 from .serializers import TestSerializer, ResultSerializer
-
-
+import openpyxl
+from urllib.parse import quote
+from django.http import HttpResponse
 
 class TestView(generics.RetrieveAPIView):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+
+class ResultView(views.APIView):
+    def get(self, request):
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+        worksheet.title = 'Отчет по тестированию'
+        columns = ['ID', 'Тест', 'Пользователь', 'Всего вопросов', 'Верных ответов']  # Замените на свои поля
+        worksheet.append(columns)
+        
+        data = Result.objects.all()
+        
+        for res in data:
+            row = [res.id, res.test.title, res.user.fio, res.total, res.score]
+            worksheet.append(row)
+        
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        filename = 'Отчет по тестированию.xlsx'
+        response['Content-Disposition'] = f'attachment; filename*=UTF-8\'\'{quote(filename)}'
+
+        # Запрещаем кэширование
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        
+        workbook.save(response) 
+        
+        return response
+        
 
 class TestCheckView(views.APIView):
     
