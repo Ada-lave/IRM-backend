50fcf7cf3741444de6d7590c0fb1c1b428cf8377
Ada lave <mega.kefi36@gmail.com>
2024-09-22 05:06:20 +0300

    Фильтрование формирования отчета по департаментам и тестам

--- irm_backend/tests/views.py
+++ irm_backend/tests/views.py
@@ -2,44 +2,43 @@ from rest_framework import generics, views
 from .models import Test, Answer, Result
 from rest_framework.response import Response
 from .serializers import TestSerializer, ResultSerializer
-import openpyxl
 from urllib.parse import quote
 from django.http import HttpResponse
+from utils.exporter import Export
+
 
 class TestView(generics.RetrieveAPIView):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
 
+
 class ResultView(views.APIView):
     def get(self, request):
-        workbook = openpyxl.Workbook()
-        worksheet = workbook.active
-        worksheet.title = 'Отчет по тестированию'
-        columns = ['ID', 'Тест', 'Пользователь', 'Всего вопросов', 'Верных ответов']  # Замените на свои поля
-        worksheet.append(columns)
-        
-        data = Result.objects.all()
-        
-        for res in data:
-            row = [res.id, res.test.title, res.user.fio, res.total, res.score]
-            worksheet.append(row)
-        
-        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        filename = 'Отчет по тестированию.xlsx'
-        response['Content-Disposition'] = f'attachment; filename*=UTF-8\'\'{quote(filename)}'
+        exporter = Export()
+        workbook = exporter.export(
+            test_id=request.query_params.get("test_id"),
+            department_id=request.query_params.get("department_id"),
+        )
+        response = HttpResponse(
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+        filename = "Отчет по тестированию.xlsx"
+        response["Content-Disposition"] = (
+            f"attachment; filename*=UTF-8''{quote(filename)}"
+        )
 
         # Запрещаем кэширование
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
-        
-        workbook.save(response) 
-        
+        response["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response["Pragma"] = "no-cache"
+        response["Expires"] = "0"
+
+        workbook.save(response)
+
         return response
-        
+
 
 class TestCheckView(views.APIView):
-    
+
     def post(self, request, pk):
         data = dict(request.data)
         test = Test.objects.get(id=data.get("id"))
@@ -48,15 +47,24 @@ class TestCheckView(views.APIView):
         for question in data["questions"]:
             total += 1
             for answer in question["answers"]:
-                if Answer.objects.get(id=answer["id"], question_id=question["id"]).is_right == True :
+                if (
+                    Answer.objects.get(
+                        id=answer["id"], question_id=question["id"]
+                    ).is_right
+                    == True
+                ):
                     score += 1
-        
-        result_serializer = ResultSerializer(data={"total": total, "score": score, "test": test.id, "user": request.user.id})
+
+        result_serializer = ResultSerializer(
+            data={
+                "total": total,
+                "score": score,
+                "test": test.id,
+                "user": request.user.id,
+            }
+        )
         result_serializer.is_valid(raise_exception=True)
         result_serializer.save()
-        
-        
-                    
+
         # data = dict(request.data.get("questions"))
         return Response(result_serializer.data)
-            
---
+++ irm_backend/utils/exporter.py
@@ -0,0 +1,35 @@
+import openpyxl
+from tests.models import Result
+
+
+class Export:
+
+    def export(self, test_id=None, department_id=None) -> openpyxl.Workbook:
+        results = Result.objects.all()
+        if test_id:
+            results = results.filter(test_id=test_id)
+
+        if department_id:
+            results = results.filter(user__department_id=department_id)
+
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+        worksheet.title = "Отчет по тестированию"
+        columns = [
+            "ID",
+            "Тест",
+            "Пользователь",
+            "Всего вопросов",
+            "Верных ответов",
+        ]
+        worksheet.append(columns)
+        worksheet.column_dimensions['B'].width = 40
+        worksheet.column_dimensions['C'].width = 35
+        worksheet.column_dimensions['D'].width = 15
+        worksheet.column_dimensions['E'].width = 15
+
+        for res in results:
+            row = [res.id, res.test.title, res.user.fio, res.total, res.score]
+            worksheet.append(row)
+
+        return workbook
